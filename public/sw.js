aa18862d983deb58e6b7a3c5594631ebe24c8330
Colm-Desktop <mail@colm.be>
2022-02-26 14:17:18 +0000

    back to original sw

--- public/sw.js
+++ public/sw.js
@@ -4,48 +4,27 @@ const filesToCache = [
   'main.js',
   'index.html',
   'assets/css/style.css',
-  'sample-pwa/',
-  'sample-pwa/main.js',
-  'sample-pwa/index.html',
-  'sample-pwa/assets/css/style.css',
+  '/sample-pwa/',
+  '/sample-pwa/main.js',
+  '/sample-pwa/index.html',
+  '/sample-pwa/assets/css/style.css',
 
 ];
 
-self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(filesToCache);
-  return self.skipWaiting();
+/* Cache contents when Offline See Cache */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', e => {
-  self.clients.claim();
+/* Serve cached content when offline, examine Cache Storage */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
-
-self.addEventListener('fetch', async e => {
-  const req = e.request;
-  const url = new URL(req.url);
-
-  if (url.origin === location.origin) {
-    e.respondWith(cacheFirst(req));
-  } else {
-    e.respondWith(networkAndCache(req));
-  }
-});
-
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cached = await cache.match(req.url);
-  return cached || fetch(req);
-}
-
-async function networkAndCache(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const fresh = await fetch(req);
-    await cache.put(req, fresh.clone());
-    return fresh;
-  } catch (e) {
-    const cached = await cache.match(req);
-    return cached;
-  }
-}
